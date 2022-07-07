d5c6666eb225b584f8e8646461aa3fef88ad954d
pavithra240 <60832639+pavithra240@users.noreply.github.com>
2022-07-07 11:40:02 +0530

    main update

--- app/src/main/java/com/example/dtrack/MainActivity.java
+++ app/src/main/java/com/example/dtrack/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.dtrack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button newbtn;
+    Button newbt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
