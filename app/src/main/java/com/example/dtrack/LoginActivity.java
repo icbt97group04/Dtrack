9274855cfeb3c4d1e7e612925513804b102a9f87
pavithra240 <60832639+pavithra240@users.noreply.github.com>
2022-07-08 10:09:07 +0530

    update by pav

--- app/src/main/java/com/example/dtrack/LoginActivity.java
+++ app/src/main/java/com/example/dtrack/LoginActivity.java
@@ -1,27 +1,40 @@
 package com.example.dtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button login,logi1;
+
+    TextView tv_lat,tv_lon,tv_altitude,tv_accuracy,tv_speed, tv_sensor, tv_updates,tv_address;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = findViewById(R.id.btnLogin);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DriverActivity.class);
-                startActivity(intent);
-            }
-        });
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
     }
 }
