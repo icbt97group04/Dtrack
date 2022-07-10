98fa76a4b4fa2d81e49bd065cd231477900751ec
ACER <dcdc1999dc@gmail.com>
2022-07-10 17:35:28 +0530

    update by darc

--- app/src/main/java/com/example/dtrack/LoginActivity.java
+++ app/src/main/java/com/example/dtrack/LoginActivity.java
@@ -2,26 +2,131 @@ package com.example.dtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Button login;
+    TextView username,password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Spinner spinner=findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.userlevel, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         login = findViewById(R.id.btnLogin);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        String tef="Driver";
+        driverlogin(tef);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        //Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+        //driverlogin(text);
+    }
+
+    private void driverlogin(String text) {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DriverActivity.class);
-                startActivity(intent);
+                String user= username.getText().toString();
+                String pw=password.getText().toString();
+                //Toast.makeText(LoginActivity.this, text, Toast.LENGTH_SHORT).show();
+                if(text=="Driver"){
+                logind(user,pw);
+
+                }
+
+
             }
         });
+
+
     }
+
+    private void logind(String user,String pw) {
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("login your account");
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.show();
+        String uRl = "https://dtrack.live/darclogin2.php";
+        StringRequest request = new StringRequest(Request.Method.POST, uRl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                if (response.equals("Login Success")){
+                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+
+
+                    startActivity(new Intent(LoginActivity.this,DriverActivity.class));
+                    progressDialog.dismiss();
+                    finish();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> param = new HashMap<>();
+                param.put("Name",user);
+                param.put("DriverPassword",pw);
+                return param;
+
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(30000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getmInstance(LoginActivity.this).addToRequestQueue(request);
+        Toast.makeText(LoginActivity.this, user+"  "+pw, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+       //Toast.makeText(adapterView.getContext(), "Select User Level", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
